177c64e34dd468cfc5e19cf6cfb365becff179e6
Raymond <raymondk25@users.noreply.github.com>
2023-06-06 22:59:19 +0800

    Add required packages & Setting up the ChatBot Application

---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,6 @@
+class AssetsManager {
+  static String imagePath = 'assets/images';
+  static String userImage = '$imagePath/person.png';
+  static String openAILogo = '$imagePath/openai_logo.jpg';
+  static String chatLogo = '$imagePath/chat_logo.png';
+}
