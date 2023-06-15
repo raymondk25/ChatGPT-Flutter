8b6e5576974f55c444b671f11d2e5b1a8971a19c
Raymond <raymondk25@users.noreply.github.com>
2023-06-15 23:56:45 +0800

    Impelement the ModalBottomSheet

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:chat_gpt/constants/constants.dart';
 import 'package:chat_gpt/services/assets_manager.dart';
 import 'package:chat_gpt/widgets/chat_widget.dart';
+import 'package:chat_gpt/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
@@ -38,7 +39,26 @@ class _ChatScreenState extends State<ChatScreen> {
         title: const Text("ChatGPT"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () async {
+              await showModalBottomSheet(
+                backgroundColor: kScaffoldBackgroundColor,
+                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+                context: context,
+                builder: (context) {
+                  return Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Row(
+                      children: const [
+                        TextWidget(
+                          label: "Choosen Model:",
+                          fontSize: 16,
+                        )
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
             icon: const Icon(
               Icons.more_vert_rounded,
               color: Colors.white,
